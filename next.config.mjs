715767888538233d3392a1f79f751d1f2405ef85
ozma <kitsune950907@gmail.com>
2024-04-22 14:09:04 +0900

    0422

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-};
+    images: {
+        unoptimized: true, // 이미지 최적화 기능 비활성화
+    },
+}
 
 export default nextConfig;
