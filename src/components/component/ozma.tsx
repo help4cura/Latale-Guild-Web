4fe38a31d1b00cf87ca779ee05d43f8b43c1ab6b
ozma <kitsune950907@gmail.com>
2024-04-23 15:32:01 +0900

    모바일 Effect 적용

--- src/components/component/ozma.tsx
+++ src/components/component/ozma.tsx
@@ -6,7 +6,7 @@
 //ozma.tsx
 
 //Import
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Link from "next/link";
 import Sidebar from './sidebar';
 import PopupImage from './popupImage';
@@ -134,6 +134,23 @@ export function _ozma() {
 }
 
 function ScaleIcon(props: SVGProps<SVGSVGElement>) {
+  const gradientRef = useRef<SVGLinearGradientElement>(null);
+
+  useEffect(() => {
+    const gradient = gradientRef.current;
+    let rotateAngle = 0;
+
+    const animate = () => {
+      rotateAngle = (rotateAngle + 1) % 360;
+      if (gradient) {
+        gradient.setAttribute('gradientTransform', `rotate(${rotateAngle}, 0.5, 0.5)`);
+      }
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+  }, []);
+
   return (
     <svg
       {...props}
@@ -145,24 +162,16 @@ function ScaleIcon(props: SVGProps<SVGSVGElement>) {
       className="w-20 h-20"
     >
       <defs>
-        <linearGradient id="animatedGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+        <linearGradient id="animatedGradient" x1="0%" y1="0%" x2="0%" y2="100%" ref={gradientRef}>
           <stop offset="0%" stopColor="#6a82fb" />
           <stop offset="100%" stopColor="#b892ff" />
-          <animateTransform
-            attributeName="gradientTransform"
-            type="rotate"
-            from="0 0.5 0.5" to="360 0.5 0.5"
-            dur="1.5s"
-            repeatCount="indefinite"
-            additive="sum"
-          />
         </linearGradient>
       </defs>
-      <g stroke="url(#animatedGradient)" strokeWidth="2">
+      <g stroke="url(#animatedGradient)" strokeWidth={2}>
         <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
         <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
         <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2 M7 21h10 M12 3v18" />
       </g>
     </svg>
   );
-}
+};
